8bce2433a673e481880012242c47cf66f7ae67f7
Sophie Feldmann <104968221+sophiehf35@users.noreply.github.com>
2024-12-29 12:42:27 -0300

    Ajuste diretorio blog

--- js/pagina-artigos.js
+++ js/pagina-artigos.js
@@ -413,7 +413,7 @@ function carregaConteudoDestaque(config) {
 
                 data.forEach(conteudo => {
                     const imagem = (conteudo.tipo === 'artigos' ? `/usuarios/${conteudo.diretorio_autor}/artigos/thumb/${conteudo.imagem_destaque}` : `/ferramentas/${conteudo.imagem_destaque}`);
-                    const slugConteudo = (config.diretorio_blog === "home" ? "" : `${config.diretorio_blog}/`) (conteudo.tipo === 'artigos' ? `${conteudo.slug_categoria}/${conteudo.slug}` : `ferramentas/${conteudo.slug}`);
+                    const slugConteudo = `${config.diretorio_blog === "home" ? "" : `${config.diretorio_blog}/`}${conteudo.tipo === 'artigos' ? `${conteudo.slug_categoria}/${conteudo.slug}` : `ferramentas/${conteudo.slug}`}`;
                     const categoria = (conteudo.tipo === 'artigos' ? conteudo.categoria.toUpperCase() : conteudo.tipo.toUpperCase());
             
                     link += `
