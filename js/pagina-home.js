ffb2e3c9aeec8e2db8abb958f5662a3b9a8b760e
Sophie Feldmann <104968221+sophiehf35@users.noreply.github.com>
2024-12-29 13:07:20 -0300

    Ajuste diretorio blog

--- js/pagina-home.js
+++ js/pagina-home.js
@@ -220,7 +220,7 @@ function carregaConteudoHomePortal(config) {
                 const colImagem = document.createElement('div');
                 colImagem.classList.add('col-xl-6', 'col-xxl-6');
                 const linkImagem = document.createElement('a');
-                linkImagem.href = '/' + item.slug_categoria + '/' + item.slug;
+                linkImagem.href = (config.diretorio_blog === 'home' ? '/' : `/${config.diretorio_blog}/`) + item.slug_categoria + '/' + item.slug;
                 const hoverContainer = document.createElement('div');
                 hoverContainer.classList.add('hover', 'hover-3', 'text-white', 'secao-imagem');
                 const imagem = document.createElement('img');
@@ -305,7 +305,7 @@ function carregaConteudoHomePortal(config) {
                 const conteudo = document.createElement('div');
                 conteudo.classList.add('conteudo', 'mb-4', 'mb-lg-3', 'mb-xl-4');
                 const tituloLink = document.createElement('a');
-                tituloLink.href = '/' + item.slug_categoria + '/' + item.slug;
+                tituloLink.href = (config.diretorio_blog === 'home' ? '/' : `/${config.diretorio_blog}/`) + item.slug_categoria + '/' + item.slug;
                 const titulo = document.createElement('h2');
                 titulo.classList.add('card-title', 'mb-3', 'h5');
                 titulo.textContent = item.titulo;
