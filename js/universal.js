18fb57b91d538766b40eef028abac74edd048941
Sophie Feldmann <104968221+sophiehf35@users.noreply.github.com>
2025-11-23 22:45:33 -0300

    Ajuste

--- js/universal.js
+++ js/universal.js
@@ -131,7 +131,7 @@ defineVariaveisUniversais(slugDaPagina).then(config => {
 
     } else if (caminho.includes('/profissionais/')) {
         // PÁGINAS DE PROFISSIONAIS
-        if (config.profissionais && config.profissionais.tipos.some(tipo => tipo.slug_diretorio && caminho.includes(`/${tipo.slug_diretorio}/`)) && caminho.split('/').filter(Boolean).length > 3) {
+        if (config.profissionais && config.profissionais.tipos.some(tipo => tipo.slug_diretorio && caminho.includes(`/${tipo.slug_diretorio}/`)) && caminho.split('/').filter(Boolean).length === 3) {
             // PÁGINAS DE CADA PROFISSIONAL
             console.log('usuario');
             validarFormularioContatoProfissional(config);
